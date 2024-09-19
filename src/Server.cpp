c8440549cf75a83f10b1371a52d1caf3fda857df
Ashfinn <obidur.shawal@gmail.com>
2024-09-19 14:38:19 +0600

    test

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
     }
-    else {
-        throw std::runtime_error("Unhandled pattern " + pattern);
+    else if (pattern == "\\d") {
+        return input_line.find_first_of("0123456789") != std::string::npos;
     }
-}
+    else if (pattern == "\\w") {
+        return std::find_if(input_line.begin(), input_line.end(), [](char c) {
+            return std::isalnum(c); }) != input_line.end();
+    }
+    else if (pattern.front() == '[' && pattern.back() == ']') {
+       
+       bool negative = pattern[1] == '^';
+       
+        std::string chars_to_match = pattern.substr(negative ? 2 : 1, pattern.size() - negative ? 3 : 2);
+    
+        for (const auto &c : input_line) {
+            if (chars_to_match.find(c) != std::string::npos && !negative) {
+                // Positive match: character is found in the group
+                return true;
+            } else if (chars_to_match.find(c) == std::string::npos && negative) {
+                // Negative match: character is not found in the group
+                return true;
+            }
+        }
 
-bool match_character_class(const std::string& input_line, const std::string& pattern) {
-    if (pattern == "\\d") {
-        return input_line.find(pattern) != std::string::npos;
+        return false;  // No match found in input
     }
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
@@ -49,9 +66,9 @@ int main(int argc, char* argv[]) {
       } else {
           return 1;
       }
-        } catch (const std::runtime_error& e) {
-            std::cerr << e.what() << std::endl;
-            return 1;
-            }
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+        }
 
 }
